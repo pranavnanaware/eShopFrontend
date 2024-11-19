8bba72709b6df435eff26b924f51a9762ecbe3c8
p-nanaware <pnanaware.umassd.edu>
2024-11-19 12:52:38 -0800

    Initial commit

---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+}
